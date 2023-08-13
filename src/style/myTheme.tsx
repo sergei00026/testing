8d99433c45473eba59bd33005d01f2d4fafd9049
sergei00026 <sergei57fx@mail.ru>
2023-08-13 09:04:38 +0300

    first commit

---
+++ src/style/myTheme.tsx
@@ -0,0 +1,12 @@
+export const myTheme = {
+    colors:{
+        primary: 'pink',
+        secondary: 'purple',
+        grey:{
+            dark: '#4d4a4a',
+            light: '#b9b4b4'
+        }
+    },
+
+
+}
